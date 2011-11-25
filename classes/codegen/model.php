cf473f5a841e2dd6a1adb4d4e09ff50a46c98bce
sumh <oalite@gmail.com>
2011-11-25 20:52:08 +0800

    Move model generate template into separate view file.
    make parameter of append($param) and update($param) is $_POST else $_GET by default
    
    sumh <oalite@gmail.com>

--- classes/codegen/model.php
+++ classes/codegen/model.php
@@ -51,13 +51,14 @@ class Codegen_Model extends Codegen {
 
     protected function model($table, $columns)
     {
-        $table_old = $table;
+        $data['database']  = $this->module;
+        $data['table_old'] = $table;
 
-        $key_id = key($columns);
+        $data['key_id'] = $key_id = key($columns);
 
-        $tables     = explode('_', $table);
-        $table      = Inflector::singular(end($tables));
-        $file       = $this->repository.'model'.DIRECTORY_SEPARATOR.$table.'.php';
+        $tables = explode('_', $table);
+        $data['table'] = $table = Inflector::singular(end($tables));
+        $file = $this->repository.'model'.DIRECTORY_SEPARATOR.$table.'.php';
 
         if(file_exists($file))
         {
@@ -66,14 +67,16 @@ class Codegen_Model extends Codegen {
 
         $uctalbe    = '_'.ucfirst($table);
 
-        $content    = "<?php defined('SYSPATH') or die('No direct script access.');\n"
-                        .strtr(parent::$config['license'], array(
-                            '$package'  => $this->module,
-                            '$year'     => date('Y'),
-                            '$see'      => $this->settings['extends'],
-                        ))."\nclass {$this->settings['directory']}{$uctalbe} extends {$this->settings['extends']} {\n\n";
+        $data['description'] = strtr(parent::$config['license'], array(
+            '$package'  => $this->module,
+            '$year'     => date('Y'),
+            '$see'      => $this->settings['extends'],
+        ));
 
-        $datatime = '';
+        $data['class'] = $this->settings['directory'].$uctalbe;
+        $data['extends'] = $this->settings['extends'];
+
+        $data['datatime'] = '';
 
         $rule = $rule_insert = $rule_update = $comment = array();
 
@@ -96,7 +99,7 @@ class Codegen_Model extends Codegen {
                     $rule_insert[$key]['range'] = $rule_update[$key]['range'] = array($column['min'], $column['max']);
                     if(preg_match('/\_date$/i', $key))
                     {
-                        $datatime .= "
+                        $data['datatime'] .= "
         if(isset(\$params['$key']) AND \$timetamp = strtotime(\$params['$key']))
             \$params['$key'] = \$timetamp;
         else
@@ -141,200 +144,26 @@ class Codegen_Model extends Codegen {
             //    $rule[$last] .= ';';
         }
 
-        $rules = $rule ? "\n\t\t\t".implode("\n\t\t\t", $rule) : '';
+        $data['rules'] = $rule ? "\n\t\t\t".implode("\n\t\t\t", $rule) : '';
 
-        $comment = $comment ? "\n\t *      ".implode("\n\t *      ", $comment)."\n\t *" : '';
+        $data['comment'] = $comment ? "\n\t *      ".implode("\n\t *      ", $comment)."\n\t *" : '';
 
-        $columns    = implode('\',\'', array_keys($columns));
+        $data['columns'] = implode('\',\'', array_keys($columns));
 
-        $rule_insert = '$rules = array_intersect_key('.preg_replace(
+        $data['rule_insert'] = '$rules = array_intersect_key('.preg_replace(
                 array('#\n\s+array#m', '#\(\n\s+\'#', '#\(\n\s+\d\s=\>\s#', '#,\n\s+\d\s=\>\s#', '#,\n\s+\)#', '#\'(\d+)\'#', '#  #', '#\n#', '#,\),\)#'),
                 array('array', '(\'', "(", ',', ",)", '$1', '    ', "\n        ", '))'),
                 var_export($rule_insert, TRUE))
             .', $params);';
 
-        $rule_update = '$rules = array_intersect_key('.preg_replace(
+        $data['rule_update'] = '$rules = array_intersect_key('.preg_replace(
                 array('#\n\s+array#m', '#\(\n\s+\'#', '#\(\n\s+\d\s=\>\s#', '#,\n\s+\d\s=\>\s#', '#,\n\s+\)#', '#\'(\d+)\'#', '#  #', '#\n#', '#,\),\)#', '#=\> NULL,\n\s+#'),
                 array('array', '(\'', "(", ',', ",)", '$1', '    ', "\n        ", '))', '=> NULL, '),
                 var_export($rule_update, TRUE))
             .', $params);';
 
-        $content .= <<< CCC
-    protected \$_db = '{$this->module}';
-
-    public function get(\$$key_id)
-    {
-        return ctype_digit((string) \$$key_id)
-            ? DB::select('$columns')
-                ->from('$table_old')
-                ->where('$key_id', '=', \$$key_id)
-                ->execute(\$this->_db)
-                ->current()
-            : NULL;
-    }
-
-    /**
-     * Insert $table
-     *
-     * @access	public
-     * @param	array	\$params$comment
-     * @return	mix     Validated data or validate object
-     */
-    public function append(array \$params)
-    {{$datatime}
-        \$valid = Validate::factory(\$params)$rules;
-
-        $rule_insert
-
-        foreach(\$rules as \$field => \$rule)
-            foreach(\$rule as \$r => \$p)
-                \$valid->rule(\$field, \$r, \$p);
-
-        if(\$valid->check())
-        {
-            \$valid = \$valid->as_array();
-
-            foreach(\$valid as \$key => \$val)
-            {
-                if(\$val === '') \$valid[\$key] = NULL;
-            }
-
-            //\$valid['insert_by']     = \$_SESSION['user'];
-            \$valid['insert_time']  = \$_SERVER['REQUEST_TIME'];
-
-            \$insert = DB::insert('$table_old', array_keys(\$valid))
-                ->values(array_values(\$valid))
-                ->execute(\$this->_db);
-
-            \$valid                 += \$params;
-            \$valid['$key_id'] = \$insert[0];
-            \$valid['affected_rows'] = \$insert[1];
-        }
-
-        // Validation data, or collection of the errors
-        return \$valid;
-    }
-
-    /**
-     * Update $table
-     *
-     * @access	public
-     * @param	int	    \$$key_id
-     * @param	array	\$params$comment
-     * @return	mix     Validated data or validate object
-     */
-    public function update(\$$key_id, array \$params)
-    {{$datatime}
-        \$valid = Validate::factory(\$params);
-
-        $rule_update
-
-        foreach(\$rules as \$field => \$rule)
-            foreach(\$rule as \$r => \$p)
-                \$valid->rule(\$field, \$r, \$p);
-
-        if(\$valid->check())
-        {
-            \$valid = \$valid->as_array();
-
-            foreach(\$valid as \$key => \$val)
-            {
-                if(\$val === '') \$valid[\$key] = NULL;
-            }
-
-            //\$valid['update_by']     = \$_SESSION['user'];
-            \$valid['update_time']   = \$_SERVER['REQUEST_TIME'];
-
-            \$valid['affected_rows'] = DB::update('$table_old')
-                ->set(\$valid)
-                ->where('$key_id', '=', \$$key_id)
-                ->execute(\$this->_db);
-
-            \$valid += \$params;
-        }
-
-        // Validation data, or collection of the errors
-        return \$valid;
-    }
-
-    public function delete(\$$key_id)
-    {
-        return ctype_digit((string) \$$key_id)
-            ? DB::delete('$table_old')
-                ->where('$key_id', '=', \$$key_id)
-                ->execute(\$this->_db)
-            : NULL;
-    }
-
-    /**
-     * List {$table}s
-     *
-     * @access	public
-     * @param	array	    \$params
-     * @param	Pagination	\$pagination	default [ NULL ] passed by reference
-     * @param	boolean	    \$calc_total	default [ TRUE ] is needed to caculate the total records for pagination
-     * @return	array       array('{$table}s' => data, 'orderby' => \$params['orderby'], 'pagination' => \$pagination)
-     */
-    public function lists(array \$params, \$pagination = NULL, \$calc_total = TRUE)
-    {
-        \$pagination instanceOf Pagination OR \$pagination = new Pagination;
-
-        \$sql = 'FROM `$table_old` ';
-
-        // Customize where from params
-        //\$sql .= 'WHERE ... '
-
-        // caculte the total rows
-        if(\$calc_total === TRUE)
-        {
-            \$pagination->total_items = \$this->_db->query(
-                Database::SELECT, 'SELECT COUNT(`$key_id`) num_rows '.\$sql, FALSE
-            )->get('num_rows');
-
-            \$data['pagination'] = \$pagination;
-
-            if(\$pagination->total_items === 0)
-            {
-                \$data['{$table}s'] = array();
-                isset(\$params['orderby']) AND \$data['orderby'] = \$params['orderby'];
-                return \$data;
-            }
-        }
-
-        // Customize order by from params
-        if(isset(\$params['orderby']))
-        {
-            switch(\$params['orderby'])
-            {
-                case 'priority':
-                    \$sql .= ' ORDER BY priority DESC';
-                    break;
-                case 'status':
-                    \$sql .= ' ORDER BY status DESC';
-                    break;
-                case 'update':
-                    \$sql .= ' ORDER BY update_time DESC';
-                    break;
-                default:
-                    \$params['orderby'] = 'priority';
-                    \$sql .= ' ORDER BY priority DESC';
-                    break;
-            }
-            \$data['orderby'] = \$params['orderby'];
-        }
-
-        \$sql .= " LIMIT {\$pagination->offset}, {\$pagination->items_per_page}";
-
-        \$data['{$table}s'] = \$this->_db->query(Database::SELECT, 'SELECT * '.\$sql, FALSE);
-
-        return \$data;
-    }
-
-} // END {$this->settings['directory']}$uctalbe
-
-CCC;
         $fp = fopen($file, 'w');
-        fwrite($fp, $content);
+        fwrite($fp, View::factory('codegen/model/template', $data)->render());
         fclose($fp);
 
         return TRUE;
---
+++ views/codegen/model/template.php
@@ -0,0 +1,169 @@
+<?php
+echo "<?php defined('SYSPATH') or die('No direct script access.');\n$description\n";
+?>
+class <?= $class ?> extends <?= $extends ?> {
+
+    protected $_db = '<?= $database ?>';
+
+    public function get($<?= $key_id ?>)
+    {
+        return ctype_digit((string) $<?= $key_id ?>)
+            ? DB::select('<?= $columns ?>')
+                ->from('<?= $table_old ?>')
+                ->where('<?= $key_id ?>', '=', $<?= $key_id ?>)
+                ->execute($this->_db)
+                ->current()
+            : NULL;
+    }
+    /**
+     * Insert <?= $table ?>
+     *
+     * @access	public
+     * @param	array	$params<?= $comment ?>
+     * @return	mix     Validated data or validate object
+     */
+    public function append(array $params = NULL)
+    {
+        <?= $datatime."\n        " ?>$valid = Validate::factory($params ?: $_POST ?: $_GET)<?= $rules ?>;
+
+        <?= $rule_insert."\n" ?>
+
+        foreach($rules as $field => $rule)
+            foreach($rule as $r => $p)
+                $valid->rule($field, $r, $p);
+
+        if($valid->check())
+        {
+            $valid = $valid->as_array();
+
+            foreach($valid as $key => $val)
+            {
+                if($val === '') $valid[$key] = NULL;
+            }
+
+            // $valid['insert_by']   = $_SESSION['user'];
+            $valid['insert_time']   = $_SERVER['REQUEST_TIME'];
+
+            $insert = DB::insert('<?= $table_old ?>', array_keys($valid))
+                ->values(array_values($valid))
+                ->execute($this->_db);
+
+            $valid                 += $params;
+            $valid['<?= $key_id ?>'] = $insert[0];
+            $valid['affected_rows'] = $insert[1];
+        }
+
+        // Validation data, or collection of the errors
+        return $valid;
+    }
+
+    /**
+     * Update <?= $table."\n" ?>
+     *
+     * @access	public
+     * @param	int	    $<?= $key_id ?>
+     * @param	array	$params<?= $comment."\n" ?>
+     * @return	mix     Validated data or validate object
+     */
+    public function update($<?= $key_id ?>, array $params = NULL)
+    {
+        <?= $datatime."\n        " ?>$valid = Validate::factory($params ?: $_POST ?: $_GET);
+
+        <?= $rule_update."\n" ?>
+
+        foreach($rules as $field => $rule)
+            foreach($rule as $r => $p)
+                $valid->rule($field, $r, $p);
+
+        if($valid->check())
+        {
+            $valid = $valid->as_array();
+
+            foreach($valid as $key => $val)
+            {
+                if($val === '') $valid[$key] = NULL;
+            }
+
+            //$valid['update_by']   = $_SESSION['user'];
+            $valid['update_time']   = $_SERVER['REQUEST_TIME'];
+
+            $valid['affected_rows'] = DB::update('<?= $table_old ?>')
+                ->set($valid)
+                ->where('<?= $key_id ?>', '=', $<?= $key_id ?>)
+                ->execute($this->_db);
+
+            $valid += $params;
+        }
+
+        // Validation data, or collection of the errors
+        return $valid;
+    }
+
+    public function delete($<?= $key_id ?>)
+    {
+        return ctype_digit((string) $<?= $key_id ?>)
+            ? DB::delete('<?= $table_old ?>')
+                ->where('<?= $key_id ?>', '=', $<?= $key_id ?>)
+                ->execute($this->_db)
+            : NULL;
+    }
+
+    /**
+     * List <?= $table ?>s
+     *
+     * @access	public
+     * @param	array	    $params
+     * @param	Pagination	$pagination	default [ NULL ] passed by reference
+     * @param	boolean	    $calc_total	default [ TRUE ] is needed to caculate the total records for pagination
+     * @return	array       array('<?= $table ?>s' => data, 'orderby' => $params['orderby'], 'pagination' => $pagination)
+     */
+    public function lists(array $params, $pagination = NULL, $calc_total = TRUE)
+    {
+        $pagination instanceOf Pagination OR $pagination = new Pagination;
+
+        $sql = 'FROM `<?= $table_old ?>` ';
+
+        // Customize where from params
+        //$sql .= 'WHERE ... '
+
+        // caculte the total rows
+        if($calc_total === TRUE)
+        {
+            $pagination->total_items = $this->_db->query(
+                Database::SELECT, 'SELECT COUNT(`<?= $key_id ?>`) num_rows '.$sql, FALSE
+            )->get('num_rows');
+
+            $data['pagination'] = $pagination;
+
+            if($pagination->total_items === 0)
+            {
+                $data['{$table}s'] = array();
+                isset($params['orderby']) AND $data['orderby'] = $params['orderby'];
+                return $data;
+            }
+        }
+
+        // Customize order by from params
+        if(isset($params['orderby']))
+        {
+            switch($params['orderby'])
+            {
+                case 'priority':
+                    $sql .= ' ORDER BY priority DESC';
+                    break;
+                default:
+                    $params['orderby'] = 'priority';
+                    $sql .= ' ORDER BY priority DESC';
+                    break;
+            }
+            $data['orderby'] = $params['orderby'];
+        }
+
+        $sql .= " LIMIT {$pagination->offset}, {$pagination->items_per_page}";
+
+        $data['<?= $table ?>s'] = $this->_db->query(Database::SELECT, 'SELECT * '.$sql, FALSE);
+
+        return $data;
+    }
+
+} // END <?= $class ?>
